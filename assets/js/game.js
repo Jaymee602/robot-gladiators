5c0bdb0d5e45609a2279841386dc681cd1781160
jam <jaymee602@gmail.com>
2022-03-31 14:10:06 -0700

    robot's name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! Javascript is running!")
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("Our robot's name is " + playerName)
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
