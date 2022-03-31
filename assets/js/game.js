63b61f082e17d2075c39ec6a22c808d1a5081234
jam <jaymee602@gmail.com>
2022-03-31 15:02:57 -0700

    health check

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,12 +20,23 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' & use that result to update the value in the 'playerHealth' variable.
     playerHealth = playerHealth - enemyAttack;
     // Log a resulting message to the console so we know that it worked.
     console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    // Check player's health 
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 fight();
